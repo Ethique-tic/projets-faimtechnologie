eb42f004ba485ae3f09836ab28e9dc82dda01e46
Arthur PASTEL <arthur.pastel@telecom-paristech.fr>
2016-06-15 23:28:58 +0200

    Initial commit

---
+++ assets/js/switcher.js
@@ -0,0 +1,9 @@
+	function entre() {
+		document.getElementById("cartoimage").src = "images/carto_entre.png";
+	}
+	function rech() {
+		document.getElementById("cartoimage").src = "images/carto_recherche.png";
+	}
+	function ong() {
+		document.getElementById("cartoimage").src = "images/carto_ong.png";
+	}
